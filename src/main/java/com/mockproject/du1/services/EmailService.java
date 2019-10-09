e8ee4535ae350bb810df10cf96dc6689ffd2d9e7
Administrator <palleducanh10@gmail.com>
2019-10-09 08:41:25 +0700

    update thu vien,cover excel

--- src/main/java/com/mockproject/du1/services/EmailService.java
+++ src/main/java/com/mockproject/du1/services/EmailService.java
@@ -1,8 +1,38 @@
 package com.mockproject.du1.services;
 
+import com.mockproject.du1.model.Department;
+import com.mockproject.du1.model.Users;
 import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
 
 @Service
 public class EmailService {
-
+    public List<String> coverExcellFileToArray(File file) throws IOException {
+        List<String> emails=new ArrayList<>();
+        FileInputStream inputStream = new FileInputStream(file);
+        HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
+        HSSFSheet sheet = workbook.getSheetAt(0);
+        Iterator<Row> rowIterator = sheet.iterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                emails.add(cell.getStringCellValue());
+            }
+        }
+        return emails;
+    }
 }
