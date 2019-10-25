5715439fff5db4f05d97c74d8dd5a1e62f09e41a
icedtea0000 <ngohoanglam65@gmail.com>
2019-10-25 15:17:30 +0700

    Lam-201910251517-minor changes

--- src/main/java/com/mockproject/du1/services/UsersService.java
+++ src/main/java/com/mockproject/du1/services/UsersService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
-import com.mockproject.du1.common.DataUtil;
 import com.mockproject.du1.mapper.RoleMapper;
 import com.mockproject.du1.mapper.UsersMapper;
 import com.mockproject.du1.model.Users;
